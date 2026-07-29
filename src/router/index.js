cee5380928beb1df779fe7c388e69a02c7a12d89
Kiblue0919 <quocanhhoang6@gmail.com>
2026-07-29 09:35:45 +0700

    fix: router and fileService fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,17 @@
-import {
-  createRouter,
-  createWebHistory
-} from 'vue-router'
-
+import { createRouter, createWebHistory } from 'vue-router'
 import UploadView from '../views/UploadView.vue'
 import FileView from '../views/FileView.vue'
-import HistoryView from '../views/HistoryView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
-
   routes: [
     {
       path: '/',
-      component: UploadView,
+      component: UploadView
     },
     {
       path: '/f/:code',
       component: FileView
-    }
-    ,{
-      path: '/history',
-      component: HistoryView
     }
   ]
 })
