b04264f27ca39644181cf0c0d67a3a9d38d3121a
LE THI KIM NGAN <nganltkgcd230276@fpt.edu.vn>
2026-08-02 23:34:32 +0700

    Update base URL for file service API

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://filesharingsevice-production-187e.up.railway.app/api/files'
+  baseURL: 'https://filesharingsevice-production-eb82.up.railway.app/'
 })
 
 export const uploadFile = (formData, onUploadProgress) => {
