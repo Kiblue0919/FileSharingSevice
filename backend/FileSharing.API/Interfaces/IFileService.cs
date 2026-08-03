917e64f5f12721fd68c7ca4b95e60d1e6bf95397
LE THI KIM NGAN <nganltkgcd230276@fpt.edu.vn>
2026-08-03 23:47:45 +0700

    Refactor IFileService interface method signatures

--- backend/FileSharing.API/Interfaces/IFileService.cs
+++ backend/FileSharing.API/Interfaces/IFileService.cs
@@ -4,9 +4,15 @@ namespace FileSharing.API.Interfaces;
 
 public interface IFileService
 {
-    Task<FileResponseDto> UploadFileAsync(UploadFileRequestDto request, string baseUrl);
+    Task<FileResponseDto> UploadFileAsync(
+        UploadFileRequestDto request);
+
     Task<FileResponseDto> GetFileMetadataAsync(string code);
-    Task<(Stream stream, string mimeType, string fileName)> DownloadFileAsync(string code);
+
+    Task<(Stream stream, string mimeType, string fileName)>
+        DownloadFileAsync(string code);
+
     Task DeleteFileAsync(string code);
+
     Task<List<FileResponseDto>> GetAllFilesAsync();
 }
